83c5f4f9c5c7313e1b7b783296a21889ed5d4033
ericpastor <ericpastorpadial@gmai.com>
2024-03-13 09:59:30 +0200

    Created structure

---
+++ src/app/components/blog-create/blog-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-create.component.html',
+  styleUrl: './blog-create.component.css'
+})
+export class BlogCreateComponent {
+
+}
