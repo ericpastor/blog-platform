a320a092ccc0de9441fc4893121882e656cf0bd2
ericpastor <ericpastorpadial@gmai.com>
2024-03-13 14:13:01 +0200

    Implemented environments

--- src/app/core/services/blog.service.ts
+++ src/app/core/services/blog.service.ts
@@ -2,16 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { BlogRaw } from '../../models/blog.model';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BlogService {
-  private baseUrl = 'http://localhost:3000/blo22gs';
-
   constructor(private httpClient: HttpClient) {}
 
   public getAllBlogs(): Observable<BlogRaw[]> {
-    return this.httpClient.get<BlogRaw[]>(this.baseUrl);
+    return this.httpClient.get<BlogRaw[]>(`${environment.baseUrl}`);
   }
 }
