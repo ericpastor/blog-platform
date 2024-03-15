cf3c52a6ad71ff73c1cfc0ca188ad72d0220a802
ericpastor <ericpastorpadial@gmai.com>
2024-03-15 16:59:45 +0200

    ReactiveForm for addNew and validation

--- src/app/components/blog-create/blog-create.component.ts
+++ src/app/components/blog-create/blog-create.component.ts
@@ -1,33 +1,43 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { BlogRaw } from '../../models/blog.model';
 import { BlogService } from '../../core/services/blog.service';
 
 @Component({
   selector: 'app-blog-create',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './blog-create.component.html',
   styleUrl: './blog-create.component.css',
 })
 export class BlogCreateComponent {
   @Input() updateList!: () => void;
 
-  public newTitle: string = '';
-  public newAuthor: string = '';
-  public newContent: string = '';
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    newTitle: ['', [Validators.required, Validators.minLength(4)]],
+    newAuthor: ['', [Validators.required, Validators.minLength(3)]],
+    newContent: ['', [Validators.required, Validators.minLength(4)]],
+  });
 
   private blogService = inject(BlogService);
 
   public AddNewBlog() {
     const newBlog = {
       id: Math.random(),
-      title: this.newTitle,
-      author: this.newAuthor,
-      content: this.newContent,
+      title: this.form.value.newTitle!,
+      author: this.form.value.newAuthor!,
+      content: this.form.value.newContent!,
       date: new Date(),
     };
+    this.form.markAsTouched();
     this.blogService.addNewBlog(newBlog).subscribe(() => {
       this.updateList();
     });
